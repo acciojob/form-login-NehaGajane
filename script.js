487e7703101876efdbefc1f6d9bb41e1c937f7b9
NehaGajane <137098317+NehaGajane@users.noreply.github.com>
2023-11-27 17:12:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	const firstName = document.querySelector('#name1');
+	
+	const lastName = document.querySelector('#name2');
 
+	alert(`${firstName.value} ${lastName.value}`);
 }
